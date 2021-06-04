c472895a61e18fd80fed2fa19a2352d35fdbfcd3
Aldo <aldomonteiro@gmail.com>
2021-06-04 09:10:03 -0300

    novorodape

--- src/Navegacao.js
+++ src/Navegacao.js
@@ -7,43 +7,45 @@ import Aula2 from "./Aula2.js";
 
 export default function Navegacao() {
   return (
-    <Tab.Container id="list-group-tabs-example" defaultActiveKey="#aula1">
-      <Row>
-        <Col>
-          <ListGroup horizontal>
-            <ListGroup.Item action href="#aula1">
-              Aula 1 - Disponível
-            </ListGroup.Item>
-            <ListGroup.Item disabled action href="#aula2">
-              Aula 2 - Em breve
-            </ListGroup.Item>
-            <ListGroup.Item disabled action href="#aula3">
-              Aula 3 - Em breve
-            </ListGroup.Item>
-            <ListGroup.Item disabled action href="#aula4">
-              Aula 4 - Em breve
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Tab.Content>
-            <Tab.Pane eventKey="#aula1">
-              <Aula1 />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#aula2">
-              <Aula2 />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#aula3">
-              <Youtube embedId="4VA6pB9jjCQ" />
-            </Tab.Pane>
-            <Tab.Pane eventKey="#aula4">
-              <Youtube embedId="eqaxodkF8Pc" />
-            </Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
+    <Container fluid>
+      <Tab.Container id="list-group-tabs-example" defaultActiveKey="#aula1">
+        <Row>
+          <Col>
+            <ListGroup horizontal>
+              <ListGroup.Item action href="#aula1" variant="danger">
+                Aula 1 - Disponível
+              </ListGroup.Item>
+              <ListGroup.Item disabled action href="#aula2" variant="danger">
+                Aula 2 - Em breve
+              </ListGroup.Item>
+              <ListGroup.Item disabled action href="#aula3" variant="danger">
+                Aula 3 - Em breve
+              </ListGroup.Item>
+              <ListGroup.Item disabled action href="#aula4" variante="danger">
+                Aula 4 - Em breve
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Tab.Content>
+              <Tab.Pane eventKey="#aula1">
+                <Aula1 />
+              </Tab.Pane>
+              <Tab.Pane eventKey="#aula2">
+                <Aula2 />
+              </Tab.Pane>
+              <Tab.Pane eventKey="#aula3">
+                <Youtube embedId="4VA6pB9jjCQ" />
+              </Tab.Pane>
+              <Tab.Pane eventKey="#aula4">
+                <Youtube embedId="eqaxodkF8Pc" />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </Container>
   );
 }
--- src/Rodape.js
+++ src/Rodape.js
@@ -1,16 +1,40 @@
-import Container from "react-bootstrap/esm/Container";
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { SocialIcon } from "react-social-icons";
 
 export default function Rodape() {
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          <SocialIcon url="https://www.youtube.com/c/alinemarius" />
-          <SocialIcon url="https://www.instagram.com/alinemarius01" />
-        </Col>
-      </Row>
-    </Container>
+    <footer id="footer">
+      <section>
+        <h2>Sobre mim</h2>
+        <p>
+          Sou mãe, professora, pedagoga. Acredito que desenvolver seus filhos em
+          casa é tão importante quanto mandá-los para a escola
+        </p>
+      </section>
+      <section>
+        <h2>Fale comigo</h2>
+        <dl className="alt">
+          <dt>Email</dt>
+          <dd>
+            <a href="#">aline@alinemarius.com.br</a>
+          </dd>
+        </dl>
+        <ul className="icons">
+          <li className="icons_li">
+            <SocialIcon url="https://www.instagram.com/alinemarius01/" />
+            <span className="label">Instagram</span>
+          </li>
+          <li className="icons_li">
+            <SocialIcon url="https://www.youtube.com/c/AlineMarius" />
+            <span className="label">Youtube</span>
+          </li>
+          <li className="icons_li">
+            <SocialIcon url="https://www.facebook.com/alinemarius/" />
+            <span className="label">Facebook</span>
+          </li>
+        </ul>
+      </section>
+    </footer>
   );
 }
