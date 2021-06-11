84e9af5d9ab9c7660c68b631e86a7f64d90fe63d
Aldo <aldomonteiro@gmail.com>
2021-06-10 22:01:42 -0300

    inscricoes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Image from "react-bootstrap/esm/Image";
 import logo from "./logo.jpg";
 import Navegacao from "./Navegacao";
 import Rodape from "./Rodape";
+import { Button } from "react-bootstrap";
 export default function App() {
   return (
     <Container className="App" fluid>
@@ -18,6 +19,23 @@ export default function App() {
         <Col>
           <Navegacao />
         </Col>
+      </Row>
+      <Row>
+        <span className="espaco"></span>
+      </Row>
+      <Row>
+        <h3>O momento para você salvar a educação da sua criança chegou!</h3>
+      </Row>
+      <Row>
+        <div className="d-grid gap-2">
+          <Button
+            href="https://go.hotmart.com/K53861123J"
+            variant="success"
+            size="lg"
+          >
+            Inscrições Abertas
+          </Button>
+        </div>
       </Row>
       <Row>
         <span className="espaco"></span>
